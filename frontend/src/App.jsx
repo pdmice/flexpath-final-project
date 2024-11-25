3ff09f9749082ac729660aa76be48a63acb00daa
johncfrauen-lc101 <christian.frauenhoffer.cs@launchcode.org>
2024-11-25 11:23:54 -0700

    got starter frontend code in there

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        {/* <p> tag is a placeholder, You'll need to change the tag/component type later*/}
+        <p className="navbar-brand ms-4 nav-link">You've got this!</p>
+      </nav>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
