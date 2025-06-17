a8d13bd2018a026aab98939fd8db85750e2c47d8
Joe Dod <25230978+pdmice@users.noreply.github.com>
2025-06-17 07:10:36 -0400

    forgot to hide admin link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { React, useState } from "react";
+import { React, useState , useContext} from "react";
 import { Link } from "react-router-dom";
 import LoginButton from "./buttons/LoginButton";
 import SearchSelect from "./buttons/SearchSelect";
 import EditSelect from "./buttons/EditSelect";
 import MyRoutes from "./components/generic/MyRoutes";
+import { AuthContext } from "./provider/AuthProvider";
+
 
 function App() {
   const [searchType, setSearchType] = useState("location");
   const [editType, setEditType] = useState("created");
   const [loading, setLoading] = useState(false)
   const [errorState, setErrorState] = useState(false)
+ const {authedUserName} = useContext(AuthContext)
 
   return (
     <div>
@@ -20,7 +23,11 @@ function App() {
         <Link to="/search" className="navbar-brand ms-4 nav-link fs-4">
           Search
         </Link>
-        <Link to="/AdminPage" className="navbar-brand ms-4 nav-link fs-4">
+        <Link to="/AdminPage" className="navbar-brand ms-4 nav-link fs-4"
+        style={{
+                display: authedUserName === "admin" ? "block" : "none",
+              }}
+        >
           Admin
         </Link>
         <SearchSelect search={searchType} setSearchType={setSearchType} />
